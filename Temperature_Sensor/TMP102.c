1c408dcd258f9cf6a1ba580697f888e814a46239
Gitanjali-Suresh <gitanjali.suresh05@gmail.com>
2020-04-13 20:11:49 -0600

    Testing TMP102

--- Temperature_Sensor/TMP102.c
+++ Temperature_Sensor/TMP102.c
@@ -14,7 +14,7 @@
 int main()
 {
 	char check_val[1] = {0x35};
-	char read_val[2];
+	char read_val[2] = {0};
 	int temp_file;
 	printf("\nThis is a test for the TMP102 sensor");
 	if((temp_file = open("/dev/i2c-2",O_RDWR)) < 0)
@@ -30,21 +30,26 @@ int main()
 	}
 	printf("\nSuccessfully connected to the sensor!");
 	printf("\nAttempting to write %x for POST",check_val[0]);
-	lseek(temp_file, 1, SEEK_SET);
+/*	lseek(temp_file, 1, SEEK_SET);
 	if((write(temp_file, check_val, 1)) != 1)
 	{
 		perror("\nFailed to write to the check value to the configuration register");
 		exit(EXIT_FAILURE);
-	}
-	printf("\nWrite successful!");
-	lseek(temp_file, 1, SEEK_SET);
-	if((read(temp_file, read_val, 1)) != 1)
+	} 
+	printf("\nWrite successful!"); */
+//	lseek(temp_file, 1, SEEK_SET);
+	if((read(temp_file, read_val, 2)) != 2)
 	{
 		perror("\nFailed to read the check value from the configuration register");
 		exit(EXIT_FAILURE);
 	}	
 	printf("\nThe read value at 0 is 0x%x",read_val[0]);
 	printf("\nThe read value at 1 is 0x%x",read_val[1]);
+	printf("\nThe read value at 2 is 0x%x",read_val[2]);
+	read_val[0] = 0;
+	read_val[1] = 0;
+	read_val[2] = 0;
+	printf("\nRead val before read [0] = 0x%x, [1] = 0x%x, [2] = 0x%x",read_val[0], read_val[1], read_val[2]);
 	printf("\nChecking positional read");
 	printf("\nReading temperature...");
 	if((pread(temp_file, read_val, 2, 0)) != 2)
@@ -54,6 +59,7 @@ int main()
 	}
 	printf("\nThe read value at 0 is 0x%x",read_val[0]);
 	printf("\nThe read value at 1 is 0x%x",read_val[1]);
+	printf("\nThe read value at 2 is 0x%x",read_val[2]);
 	close(temp_file);
 	printf("\nFile closed!\n");
 	return 0;
