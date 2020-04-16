d4423f2fc4531c9a69fdaef4a4a66bc53b17768a
Gitanjali-Suresh <gitanjali.suresh05@gmail.com>
2020-04-15 19:51:16 -0600

    Testing write to a file

--- Temperature_Sensor/TMP102.c
+++ Temperature_Sensor/TMP102.c
@@ -6,17 +6,28 @@
 #include <linux/i2c-dev.h>
 #include <sys/types.h>	//for lseeks
 #include <unistd.h>
-#include <stdlib.h>	//for exit status
+#include <stdlib.h>	//for exit status and for malloc
+#include <string.h>	//for strlen
+#include <stdbool.h>	//for boolean values
+#include <sys/stat.h> 	//for stat 
 
 #define DEV_ADDRESS (0x48)
-#define CONFIG_REGISTER (0x01)
 #define TEMP_REGISTER (0x00)
+
+/* Function to check if a given file exists */
+bool file_exists(char *filename)
+{
+	struct stat buffer;
+	return (stat(filename,&buffer) == 0);
+}
 
 int main()
 {
+	char *file_location = "\var\tmp\temperature";
+	char *data = malloc(30 * sizeof(char));
 	char check_val[1] = {TEMP_REGISTER};
 	char read_val[2] = {0};
-	int temp_file;
+	int temp_file, data_file;
 	int16_t digitalTemp;
 	float tempC, tempF;
 	printf("\nThis is a test for the TMP102 sensor");
@@ -39,7 +50,30 @@ int main()
 		exit(EXIT_FAILURE);
 	} 
 	printf("\nReset successful!");
-//	lseek(temp_file, 0, SEEK_SET);
+
+	if(file_exists(file_location) == true)
+	{
+		remove(file_location);
+	}
+	
+	// Check if the file exists else create a new file
+	if(file_exists(file_location) == false)
+	{
+		data_file = creat(file_location,0755);			// Creating a new file with all permissions granted to the user [4]
+		if(data_file < 0)
+		{
+			perror("Data File creation unsuccessful!");
+			exit(EXIT_FAILURE);
+		}
+	}
+	data_file = open(file_location, O_RDWR|O_APPEND);		
+	// The file is opened in APPEND mode to allow for consecutive writes without overwriting the previous data
+	if(data_file < 0)
+	{
+		perror("Data File open unsuccessful!");
+		exit(EXIT_FAILURE);
+	}
+
 	while(1)
 	{
 		if((read(temp_file, read_val, 2)) != 2)
@@ -56,10 +90,16 @@ int main()
 		}
 		tempC = digitalTemp * 0.0625;
 		tempF = (tempC * (9/5)) + 32;
-		printf("\nThe temperature in C is %fC and in F is %fF", tempC, tempF);
+		sprintf(data, "\nThe temperature in C is %fC and in F is %fF.", tempC, tempF);
+		if(write(data_file, data, strlen(data)) == -1)
+		{
+			perror("Write to data file failed!");
+			exit(EXIT_FAILURE);
+		}
 		sleep(1);
 	}
 	close(temp_file);
+	close(data_file);
 	printf("\nFile closed!\n");
 	return 0;
 }
