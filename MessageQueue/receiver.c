ca6f7a7d4fc2826610528f994227796925def2ad
Gitanjali-Suresh <gitanjali.suresh05@gmail.com>
2020-04-20 20:08:41 -0600

    Message Queue Test v4

--- MessageQueue/receiver.c
+++ MessageQueue/receiver.c
@@ -3,11 +3,12 @@
 #include <stdio.h> 
 #include <sys/ipc.h> 
 #include <sys/msg.h> 
+#include <stdlib.h>
 
 // structure for message queue 
 struct mesg_buffer { 
 	long mesg_type; 
-	char mesg_text[100]; 
+	char *mesg_text; 
 } message; 
 
 int main() 
@@ -24,6 +25,7 @@ int main()
 
 	while(1)
 	{
+		message.mesg_text = (char *)malloc(100 * sizeof(char));
 		// msgrcv to receive message 
 		msgrcv(msgid, &message, sizeof(message), n, 0); 
 
@@ -31,6 +33,7 @@ int main()
 		printf("Data Received is : %s \n", 
 						message.mesg_text); 
 		n++;
+		free(message.mesg_text);
 	}
 
 	// to destroy the message queue 
--- MessageQueue/sender.c
+++ MessageQueue/sender.c
@@ -3,11 +3,12 @@
 #include <stdio.h> 
 #include <sys/ipc.h> 
 #include <sys/msg.h> 
+#include <stdlib.h>
 
 // structure for message queue 
 struct mesg_buffer { 
 	long mesg_type; 
-	char mesg_text[100]; 
+	char *mesg_text; 
 } message; 
 
 int main() 
@@ -24,10 +25,9 @@ int main()
 	while(1)
 	{
 		message.mesg_type = n; 
-
+		message.mesg_text = (char *)malloc(100 * sizeof(char));
 		printf("Write Data : "); 
 		while((message.mesg_text[i++] = getchar()) != '\n');
-		//gets(message.mesg_text); 
 
 		// msgsnd to send message 
 		msgsnd(msgid, &message, sizeof(message), 0); 
@@ -36,6 +36,7 @@ int main()
 		printf("Data send is : %s \n", message.mesg_text); 
 		n++;
 		i = 0;
+		free(message.mesg_text);
 	}
 
 	return 0; 
