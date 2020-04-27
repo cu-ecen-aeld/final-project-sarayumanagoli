a4709025db262a64b53ecc537539eb4cbd9793df
Gitanjali-Suresh <gitanjali.suresh05@gmail.com>
2020-04-27 17:56:21 -0600

    Added signal handler

--- SocketClient_Integrated/client.c
+++ SocketClient_Integrated/client.c
@@ -1,9 +1,10 @@
-/**************************************************************************************
+/******************************************************************************************
 * Client TCP Program
-* @file - client.c
+* Filename - client.c
+* About - This application is responsible for sending the sensor data to the server socket
 * Reference: https://www.geeksforgeeks.org/tcp-server-client-implementation-in-c/
 *
-***************************************************************************************/
+*******************************************************************************************/
 #include <netdb.h> 
 #include <stdio.h> 
 #include <stdlib.h> 
@@ -13,6 +14,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/stat.h> 	//for stat and umask
+#include <signal.h>
 #include <stdbool.h>
 #include <fcntl.h>
 #include <syslog.h>
@@ -22,12 +24,43 @@
 #define SA struct sockaddr 
 
 bool daemon_flag = false;
+bool signal_flag = false;
+int sockfd;
 
 /* Function to check if a given file exists */
 bool file_exists(char *filename)
 {
 	struct stat buffer;
 	return (stat(filename,&buffer) == 0);
+}
+
+/* Handler for the signals */
+void sig_handler(int signo)
+{
+	if(signo == SIGINT)
+	{
+		syslog(LOG_INFO,"Caught SIGINT!");
+		printf("\nCaught SIGINT!\n");
+		signal_flag = true;
+		// After chatting close the socket 
+		close(sockfd); 
+		system("rm /var/tmp/temperature");
+	}
+	else if(signo == SIGTERM)
+	{
+		syslog(LOG_INFO,"Caught SIGTERM!");
+		printf("\nCaught SIGTERM!\n");
+		signal_flag = true;
+		// After chatting close the socket 
+		close(sockfd); 
+		system("rm /var/tmp/temperature");
+	}
+	else
+	{
+		syslog(LOG_INFO,"Unexpected signal!");
+		fprintf(stderr, "\nUnexpected signal!\n");
+		exit(EXIT_FAILURE);
+	}
 }
 
 void read_from_file(int sockfd)
@@ -52,7 +85,7 @@ void read_from_file(int sockfd)
 		}
 		printf("\nFile opened successfully!");
 		read_data = (char *)malloc(100 *sizeof(char));
-		while((fgets(read_data, 100, data_file)) != NULL)
+		while((fgets(read_data, 100, data_file)) != NULL && signal_flag != true)
 		{
 			printf("\n To Server: %s",read_data);
 			ret_val = write(sockfd, read_data, (strlen(read_data))); 	// Sending the read packets to the client socket
@@ -73,9 +106,9 @@ void read_from_file(int sockfd)
 
 
 int main(int argc, char *argv[2]) 
-{ 
-	int sockfd; 
+{  
 	struct sockaddr_in servaddr; 
+	struct sigaction sa;
 	const char *daemon = NULL;
 	pid_t pid, sid;
 
@@ -135,9 +168,26 @@ int main(int argc, char *argv[2])
 		}
 	}
 
+	// Signal implementations
+	sa.sa_handler = sig_handler;
+	sa.sa_flags = SA_RESTART;
+	sigemptyset(&sa.sa_mask);
+
+	if (sigaction(SIGINT, &sa, NULL) == -1) 
+	{
+		perror("sigaction: SIGINT");
+		exit(EXIT_FAILURE);
+        } 
+	if (sigaction(SIGTERM, &sa, NULL) == -1) 
+	{
+		perror("sigaction: SIGTERM");
+		exit(EXIT_FAILURE);
+        }
+
 	// socket create and varification 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-	if (sockfd == -1) { 
+	if (sockfd == -1) 
+	{ 
 		printf("socket creation failed...\n"); 
 		exit(0); 
 	} 
@@ -151,7 +201,8 @@ int main(int argc, char *argv[2])
 	servaddr.sin_port = htons(PORT); 
 
 	// connect the client socket to server socket 
-	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
+	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0)
+	{ 
 		printf("connection with the server failed...\n"); 
 		exit(0); 
 	} 
